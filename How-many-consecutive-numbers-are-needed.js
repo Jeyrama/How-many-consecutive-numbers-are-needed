e93082c411dfeccc3b0d57f40e2eebf4d9a68bd3
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-08-12 10:35:53 -0700

    Update How-many-consecutive-numbers-are-needed.js

--- How-many-consecutive-numbers-are-needed.js
+++ How-many-consecutive-numbers-are-needed.js
@@ -24,3 +24,15 @@ function consecutive(array) {
 }
 
 // or
+
+function consecutive(arr) {
+  let sorted = arr.sort(function(a, b) {return a-b;});
+  let count = 0;
+  for (let i = 0; i < sorted.length - 1; i++) {
+    let diff = sorted[i+1] - sorted[i]
+    if (diff > i) {
+      count += diff - 1
+    }
+  }
+  return count;
+}
